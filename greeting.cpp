34805032c8b8ca55d096dbc1b6cf6d8878a26e84
akrha98 <arkhasayoga.m@gmail.com>
2022-03-15 22:34:30 +0700

    topik A

--- greeting.cpp
+++ greeting.cpp
@@ -9,7 +9,7 @@ int main(void) {
     int comm_sz;
     int my_rank;
     MPI_Init(NULL, NULL);
-    MPI_Comm_size(MPI_COMM_WORLD, $comm_sz);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     if (my_rank != 0) {
