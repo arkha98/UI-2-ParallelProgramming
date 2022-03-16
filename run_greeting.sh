d40489223ac613a7fb22012427c5a1b8d228395e
arkha98 <arkhasayoga.m@gmail.com>
2022-03-16 09:10:21 +0700

    contoh script

--- run_greeting.sh
+++ run_greeting.sh
@@ -1,10 +1,8 @@
-echo "1 core"
-
+echo "4 core"
 start=`date +%s.%N`
 
-mpirun greeting.o --use-hwthread-cpus 1
+mpirun greeting.o --use-hwthread-cpus 4
 
 end=`date +%s.%N`
-
 runtime=$( echo "$end - $start" | bc -l )
 echo $runtime
