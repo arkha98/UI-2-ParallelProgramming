7dda2c0fd5cc186489b7caecf73b6431a2ca19fd
arkha98 <arkhasayoga.m@gmail.com>
2022-03-22 14:56:27 +0700

    edit script

--- run_greeting.sh
+++ run_greeting.sh
@@ -1,7 +1,7 @@
 echo "4 core"
 start=`date +%s.%N`
 
-mpirun greeting.o --mca opal_warn_on_missing_libcuda 0 -NP 4 --use-hwthread-cpus 4
+mpiexec greeting.o --mca opal_warn_on_missing_libcuda 0 -n 10 --use-hwthread-cpus 4
 
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
