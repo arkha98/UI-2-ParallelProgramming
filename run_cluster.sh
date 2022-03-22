5b83c919f593d396a19f63f0d891496ba2443e24
arkha98 <arkhasayoga.m@gmail.com>
2022-03-22 15:17:54 +0700

    edit script

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH -o greeting.out
 #SBATCH -p batch
-#SBATCH -N 3
+
 #SBATCH -nodelist=node-01,node-03,node-05
 
-echo "4 core"
+echo "24 core"
 start=`date +%s.%N`
 
 mpirun --mca btl_tcp_if_exclude docker0,lo -np 24 /home/user20/UI-2-ParallelProgramming/greeting.o
