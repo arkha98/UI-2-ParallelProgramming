d89e5dde5225da657cf02eb7a07b8309ab01d079
arkha98 <arkhasayoga.m@gmail.com>
2022-03-22 15:20:03 +0700

    edit script

--- run_cluster.sh
+++ run_cluster.sh
@@ -7,7 +7,7 @@
 echo "24 core"
 start=`date +%s.%N`
 
-mpirun --mca btl_tcp_if_exclude docker0,lo -np 24 /home/user20/UI-2-ParallelProgramming/greeting.o
+mpirun --mca btl_tcp_if_exclude docker0,lo -np 24 --use-hwthread-cpus 24 /home/user20/UI-2-ParallelProgramming/greeting.o
 
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
