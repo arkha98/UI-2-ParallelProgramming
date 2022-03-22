54e7b003fc3d6e60d288110073dd12bb1aafb9e5
arkha98 <arkhasayoga.m@gmail.com>
2022-03-23 01:12:54 +0700

    edit script

---
+++ my_matmul.c
@@ -0,0 +1,6 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define SIZE 16
+
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
--- run_greeting.sh
+++ run_greeting.sh
@@ -1,7 +1,7 @@
 echo "4 core"
 start=`date +%s.%N`
 
-mpiexec greeting.o --mca opal_warn_on_missing_libcuda 0 -n 10 --use-hwthread-cpus 4
+mpirun greeting.o --mca opal_warn_on_missing_libcuda 0 --hostflie yoga-hosts -np 10 --use-hwthread-cpus 4
 
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
