a3b593aaf39feff36ee77cce10844ff8fae5b2ef
arkha98 <arkhasayoga.m@gmail.com>
2022-03-23 08:05:35 +0700

    edit script

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+#SBATCH -p greeting.out
+#SBATCH -p batch
+#SBATCH -N 3
+#SBATCH --nodelist=node-01,node-03,node-05
 mpicc greeting.c -o greeting.o
-time mpirun --hostfile cluster-fasilkom-hosts -np 25 /home/user20/UI-2-ParallelProgramming/greeting.o --nodelist node-01,node-03,node-05 --use-hwthread-cpus 10
+time mpirun --hostfile cluster-fasilkom-hosts -np 10 /home/user20/UI-2-ParallelProgramming/greeting.o --use-hwthread-cpus 8 --mca btl_tcp_if_exclude docker0,lo
