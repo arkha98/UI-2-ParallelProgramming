f466ed1051f1678f68ff076a886381db67349091
arkha98 <arkhasayoga.m@gmail.com>
2022-03-23 08:01:46 +0700

    edit script

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,2 +1,2 @@
 mpicc greeting.c -o greeting.o
-time mpirun --hostfile cluster-fasilkom-hosts -p batch -N 3 -np 24 /home/user20/UI-2-ParallelProgramming/greeting.o --nodelist=node-01,node-03,node-05 --use-hwthread-cpus 1
+time mpirun --hostfile cluster-fasilkom-hosts -np 25 /home/user20/UI-2-ParallelProgramming/greeting.o --nodelist node-01,node-03,node-05 --use-hwthread-cpus 10
