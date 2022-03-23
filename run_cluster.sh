ac875e34f8d8bd530731f3f6574c58eada2f04b7
arkha98 <arkhasayoga.m@gmail.com>
2022-03-23 07:37:38 +0700

    edit script

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-#SBATCH -o greeting.out
-#SBATCH --hostfile cluster-fasilkom-hosts
-#SBATCH -p batch
-#SBATCH -N 3
-#SBATCH --nodelist=node-01,node-03,node-05
-
 echo "24 core"
 start=`date +%s.%N`
 
-mpirun -np 24 /home/user20/UI-2-ParallelProgramming/greeting.o
+mpirun --hostfile cluster-fasilkom-hosts --nodelist=node-01,node-03,node-05 -p batch -N 3 -np 24 /home/user20/UI-2-ParallelProgramming/matmul.o
 
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
