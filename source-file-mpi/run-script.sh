c816c2e6a820628e9cf26464221a6f5a89e30592
arkha98 <arkhasayoga.m@gmail.com>
2022-03-23 08:39:53 +0700

    edit script

--- source-file-mpi/run-script.sh
+++ source-file-mpi/run-script.sh
@@ -4,4 +4,4 @@
 #SBATCH -N 3
 #SBATCH --nodelist=node-01,node-02
 
-mpirun --mca btl_tcp_if_exclude docker0,lo -np 24 /home/user20/UI-2-ParallelProgramming/source-file-mpi/matmul.o
+mpirun --mca btl_tcp_if_exclude docker0,lo -np 8 /home/user20/UI-2-ParallelProgramming/source-file-mpi/matmul.o
