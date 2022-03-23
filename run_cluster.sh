a72d12ac6fa7c9d54210ee45df448ed49e35a02a
arkha98 <arkhasayoga.m@gmail.com>
2022-03-23 07:48:02 +0700

    edit script

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,9 +1,2 @@
-#!/bin/sh
-echo "24 core"
-start=`date +%s.%N`
-
-mpirun --hostfile cluster-fasilkom-hosts --nodelist=node-01,node-03,node-05 -p batch -N 3 -np 24 /home/user20/UI-2-ParallelProgramming/matmul.o
-
-end=`date +%s.%N`
-runtime=$( echo "$end - $start" | bc -l )
-echo $runtime
+mpicc greeting.c -o greeting.o
+time mpirun --hostfile=cluster-fasilkom-hosts --nodelist=node-01,node-03,node-05 -p batch -N 3 -np 24 /home/user20/UI-2-ParallelProgramming/matmul.o
--- run_greeting.sh
+++ run_greeting.sh
@@ -1,8 +1,2 @@
-echo "4 core"
-start=`date +%s.%N`
-
-mpirun greeting.o --mca opal_warn_on_missing_libcuda 0 --hostflie yoga-hosts -np 10 --use-hwthread-cpus 4
-
-end=`date +%s.%N`
-runtime=$( echo "$end - $start" | bc -l )
-echo $runtime
+mpicc greeting.c -o greeting.o
+time mpirun --hostfile yoga-hosts -np 10 greeting.o --mca opal_warn_on_missing_libcuda 0 --use-hwthread-cpus 1
