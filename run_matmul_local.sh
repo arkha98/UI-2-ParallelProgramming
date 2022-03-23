c9855f3d4dcb2f6f880e68d4687e75e3b4cb84b1
arkha98 <arkhasayoga.m@gmail.com>
2022-03-23 08:16:57 +0700

    edit script

--- run_matmul_local.sh
+++ run_matmul_local.sh
@@ -1,2 +1,2 @@
 mpicc matmul.c -o matmul.o
-time mpirun --hostfile yoga-hosts -np 8 matmul.o --mca opal_warn_on_missing_libcuda 0 --use-hwthread-cpus 1
+time mpirun --hostfile yoga-hosts -np 1 matmul.o --mca opal_warn_on_missing_libcuda 0 --use-hwthread-cpus 1
