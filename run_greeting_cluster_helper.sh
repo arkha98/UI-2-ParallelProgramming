438622a63d547a9df668bb4c62542f9f20255724
arkha98 <arkhasayoga.m@gmail.com>
2022-03-27 01:01:14 +0700

    greeting

--- run_greeting_cluster_helper.sh
+++ run_greeting_cluster_helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -o greeting.out
 #SBATCH -p batch
-#SBATCH -N 3
-#SBATCH --nodelist=node-01,node-03,node-05
+#SBATCH -N 8
+#SBATCH --nodelist=node-01,node-02,node-03,node-04,node-05,node-06,node-07,node-08
 
-time mpirun --mca btl_tcp_if_exclude docker0,lo -np 10 greeting.o
+time mpirun --mca btl_tcp_if_exclude docker0,lo -np 64 greeting.o
--- run_greeting_google.sh
+++ run_greeting_google.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mpicc greeting.c -o greeting.o
-time mpirun --hostfile google-hosts -np 2 greeting.o
+time mpirun --hostfile google-hosts -np 1 greeting.o
