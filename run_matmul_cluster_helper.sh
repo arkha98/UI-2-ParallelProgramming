df6a370bd8b53c0fa92226878003d1e56dfba49c
arkha98 <arkhasayoga.m@gmail.com>
2022-03-27 01:59:02 +0700

    matmul

--- run_matmul_cluster_helper.sh
+++ run_matmul_cluster_helper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#SBATCH -o greeting.out
 #SBATCH -p batch
 #SBATCH -N 8
 #SBATCH --nodelist=node-01,node-02,node-03,node-04,node-05,node-06,node-07,node-08
 
-time mpirun --mca btl_tcp_if_exclude docker0,lo -np 64 matmul.o
+time mpirun --mca btl_tcp_if_exclude docker0,lo -np 64 matmul.o > matmul.out
