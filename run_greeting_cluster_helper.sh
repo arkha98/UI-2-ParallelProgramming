e34a6b6eba0c30505d6d706a8e212851a95a812c
arkha98 <arkhasayoga.m@gmail.com>
2022-03-27 02:01:53 +0700

    matmul

--- run_greeting_cluster_helper.sh
+++ run_greeting_cluster_helper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#SBATCH -o greeting.out
 #SBATCH -p batch
 #SBATCH -N 8
 #SBATCH --nodelist=node-01,node-02,node-03,node-04,node-05,node-06,node-07,node-08
 
-time mpirun --mca btl_tcp_if_exclude docker0,lo -np 64 greeting.o > greeting.out
+time mpirun --mca btl_tcp_if_exclude docker0,lo -np 64 greeting.o
