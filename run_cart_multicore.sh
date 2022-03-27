fa1e72b6046dde8110d308302b50b8a2c701ccd8
arkha98 <arkhasayoga.m@gmail.com>
2022-03-27 11:14:08 +0700

    cart and graph

--- run_cart_multicore.sh
+++ run_cart_multicore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mpicc mpi_cart_create.c -o mpi_cart_create.o
-time mpirun --hostfile multicore-hosts -np 64 mpi_cart_create.o > mpi_cart_create.out
+time mpirun --hostfile multicore-hosts -np 12 mpi_cart_create.o > mpi_cart_create.out
--- run_graph_cluster_helper.sh
+++ run_graph_cluster_helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -o mpi_cart_create.out
+#SBATCH -o mpi_graph_create.out
 #SBATCH -p batch
 #SBATCH -N 8
 #SBATCH --nodelist=node-01,node-02,node-03,node-04,node-05,node-06,node-07,node-08
 
-time mpirun --mca btl_tcp_if_exclude docker0,lo -np 12 mpi_cart_create.o
+time mpirun --mca btl_tcp_if_exclude docker0,lo -np 4 mpi_graph_create.o
