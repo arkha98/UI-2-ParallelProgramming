3ead038590c74a1c41ed75be460675ce174fb9a2
arkha98 <arkhasayoga.m@gmail.com>
2022-04-01 17:42:38 +0700

    sh output

--- run_cart_google.sh
+++ run_cart_google.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#SBATCH -o mpi_cart_create.out
 mpicc mpi_cart_create.c -o mpi_cart_create.o
-time mpirun --hostfile google-hosts -np 12 mpi_cart_create.o
+$(time mpirun --hostfile google-hosts -np 12 mpi_cart_create.o) > mpi_cart_create.out
