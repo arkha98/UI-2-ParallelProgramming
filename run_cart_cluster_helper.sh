26a973c4a0148a72f021f0534838e27f37a76281
arkha98 <arkhasayoga.m@gmail.com>
2022-04-01 17:20:28 +0700

    cluster

--- run_cart_cluster_helper.sh
+++ run_cart_cluster_helper.sh
@@ -4,4 +4,4 @@
 #SBATCH -N 8
 #SBATCH --nodelist=node-01,node-02,node-03,node-04,node-05,node-06,node-07,node-08
 
-time mpirun --mca btl_tcp_if_exclude docker0,lo -np 12 mpi_cart_create.o
+time mpirun --hostfile cluster-fasilkom-hosts -np 12 mpi_cart_create.o --mca btl_tcp_if_exclude docker0,lo 
