51919e8d067bd4edb7a572f4d3a18ce2702ca282
arkha98 <arkhasayoga.m@gmail.com>
2022-04-02 08:14:08 +0700

    cluster matmul

--- run_matmul_cluster_helper.sh
+++ run_matmul_cluster_helper.sh
@@ -4,4 +4,4 @@
 #SBATCH -N 8
 #SBATCH --nodelist=node-01,node-02,node-03,node-04,node-05,node-06,node-07,node-08
 
-{ time mpirun --hostfile cluster-fasilkom-hosts -np 64 matmul_2048.o > matmul_2048_n64.out ; } 2> matmul_2048_n64.time
+{ time mpirun --hostfile cluster-hosts -np 64 matmul_2048.o > matmul_2048_n64.out ; } 2> matmul_2048_n64.time
