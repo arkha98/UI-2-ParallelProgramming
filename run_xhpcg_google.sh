eb33b01502fd182228dc36508abc6392a4779ac0
arkha98 <arkhasayoga.m@gmail.com>
2022-04-03 15:44:13 +0700

    hpcg

--- run_xhpcg_google.sh
+++ run_xhpcg_google.sh
@@ -1,5 +1,5 @@
-mkdir /home/arkha_sayoga/UI-2-ParallelProgramming/hpcg/build_linux
-cd /home/arkha_sayoga/UI-2-ParallelProgramming/hpcg/build_linux
-/home/arkha_sayoga/UI-2-ParallelProgramming/hpcg/configure Linux_MPI
+mkdir /home/sebelum_masehi/UI-2-ParallelProgramming/hpcg/build_linux
+cd /home/sebelum_masehi/UI-2-ParallelProgramming/hpcg/build_linux
+/home/sebelum_masehi/UI-2-ParallelProgramming/hpcg/configure Linux_MPI
 make
-mpirun --hostfile /home/arkha_sayoga/UI-2-ParallelProgramming/google-hosts -np 2 ./bin/xhpcg
+mpirun --hostfile /home/sebelum_masehi/UI-2-ParallelProgramming/google-hosts -np 2 ./bin/xhpcg
