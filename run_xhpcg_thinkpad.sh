a914ba98c1e6821ee57c521d5ff21c647bad7e28
arkha98 <arkhasayoga.m@gmail.com>
2022-04-03 11:08:02 +0700

    hpcg

--- run_xhpcg_thinkpad.sh
+++ run_xhpcg_thinkpad.sh
@@ -2,4 +2,4 @@ mkdir /home/sebelum_masehi/UI-2-ParallelProgramming/hpcg/build_linux
 cd /home/sebelum_masehi/UI-2-ParallelProgramming/hpcg/build_linux
 /home/sebelum_masehi/UI-2-ParallelProgramming/hpcg/configure Linux_MPI
 make
-mpirun --hostfile /home/sebelum_masehi/UI-2-ParallelProgramming/yoga-thinkpad-hosts -np 4 ./bin/xhpcg
+mpirun --hostfile /home/sebelum_masehi/UI-2-ParallelProgramming/yoga-thinkpad-hosts -np 4 ./bin/xhpcg --mca opal_warn_on_missing_libcuda 0
