635585753d2fcb3f1fb5ff5031efb4d1887fdb8d
arkha98 <arkhasayoga.m@gmail.com>
2022-04-05 17:11:03 +0700

    hpcg

--- run_xhpcg_cluster.sh
+++ run_xhpcg_cluster.sh
@@ -7,5 +7,5 @@ mkdir /home/user20/UI-2-ParallelProgramming/hpcg/build_linux
 cd /home/user20/UI-2-ParallelProgramming/hpcg/build_linux
 /home/user20/UI-2-ParallelProgramming/hpcg/configure Linux_MPI
 make
-cp /home/user20/UI-2-ParallelProgramming/hpcg.dat ./bin/
+cp /home/user20/UI-2-ParallelProgramming/hpcg.dat /home/user20/UI-2-ParallelProgramming/hpcg/build_linux/bin/
 mpirun --hostfile /home/user20/UI-2-ParallelProgramming/cluster-hosts -np 64 ./bin/xhpcg --mca btl_tcp_if_exclude docker0,lo
