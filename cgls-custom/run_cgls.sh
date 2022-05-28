6585a84e26c0aa526cd82a86d7b1126d32b8d7bb
arkha98 <arkhasayoga.m@gmail.com>
2022-05-28 14:02:17 +0700

    cgls

--- cgls-custom/run_cgls.sh
+++ cgls-custom/run_cgls.sh
@@ -9,12 +9,12 @@ make cgls_2048
 make cgls_4096
 make cgls_8192
 
-./cgls_128
-./cgls_256
-./cgls_512
-./cgls_1024
-./cgls_2048
-./cgls_4096
-./cgls_8192
+./cgls_128 > cgls_128.out
+./cgls_256 > cgls_256.out
+./cgls_512 > cgls_512.out
+./cgls_1024 > cgls_1024.out
+./cgls_2048 > cgls_2048.out
+./cgls_4096 > cgls_4096.out
+./cgls_8192 > cgls_8192.out
 
 make clean
